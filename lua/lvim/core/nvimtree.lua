da206899673df7d0257f9ee2f9e4501c419c6c31
rodvicj <rodvicj@gmail.com>
2023-10-21 10:26:43 +0800

    add nvimtree preview

--- lua/lvim/core/nvimtree.lua
+++ lua/lvim/core/nvimtree.lua
@@ -255,6 +255,8 @@ local function on_attach(bufnr)
     ["v"] = { api.node.open.vertical, opts "Open: Vertical Split" },
     ["h"] = { api.node.navigate.parent_close, opts "Close Directory" },
     ["C"] = { api.tree.change_root_to_node, opts "CD" },
+    ["<S-k>"] = { api.node.open.preview, opts "Open Preview" },
+    -- ["<S-Tab>"] = { api.tree.collapse_all, opts "Collapse All" },
     ["gtg"] = { telescope_live_grep, opts "Telescope Live Grep" },
     ["gtf"] = { telescope_find_files, opts "Telescope Find File" },
   }
